f7dab1813ff8cee047a124d8e9825bac0fab7be3
takeshi0311 <fabio.1993.2019@gmail.com>
2020-05-05 00:21:13 +0900

    修正２

--- app/controllers/linebots_controller.rb
+++ app/controllers/linebots_controller.rb
@@ -37,7 +37,7 @@ class LinebotsController < ApplicationController
   end
 
   def search_and_create_message(input)
-    RakutenWebService.configuration do |c|
+    RakutenWebService.configure do |c|
       c.application_id = ENV['RAKUTEN_APPID']
       c.affiliate_id = ENV['RAKUTEN_AFID']
     end
